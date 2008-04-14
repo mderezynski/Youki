acbb9e4b9d6d467eaa96690055bd26fe5db64589
milosz@HAL <milosz@HAL>
2008-04-15 00:50:32 +0200

    * Fix indentation

--- python/plugins/notify/__init__.py
+++ python/plugins/notify/__init__.py
@@ -12,8 +12,8 @@ class Dummy(mpx.Plugin):
     def activate(self,player,mcs):
         print ">> Notify Plugin activated"
         self.player = player
-	self.new_track = self.player.gobj().connect("new-track", self.now_playing)
-	pynotify.init("MPX")
+        self.new_track = self.player.gobj().connect("new-track", self.now_playing)
+        pynotify.init("MPX")
         return True
 
     def deactivate(self):
@@ -21,21 +21,21 @@ class Dummy(mpx.Plugin):
         self.player = None
 
     def run(self):
-        print ">> Dummy Plugin running"
+        print ">> Notify Plugin running"
 
     def now_playing(self, blah):
 
-	m = self.player.get_metadata()
+        m = self.player.get_metadata()
 
-	if m.get(mpx.AttributeId.ARTIST).is_initialized() and m.get(mpx.AttributeId.TITLE).is_initialized():
-		p_artist = m.get(mpx.AttributeId.ARTIST).val().get_string()
-		p_title = m.get(mpx.AttributeId.TITLE).val().get_string()
+        if m.get(mpx.AttributeId.ARTIST).is_initialized() and m.get(mpx.AttributeId.TITLE).is_initialized():
+            p_artist = m.get(mpx.AttributeId.ARTIST).val().get_string()
+            p_title = m.get(mpx.AttributeId.TITLE).val().get_string()
 
-		message = "<b>" + p_artist + "</b> - " + p_title
-		n = pynotify.Notification( "MPX", message)
-		n.set_urgency(pynotify.URGENCY_NORMAL)
-        image = m.get_image()
-        if(image):
-            n.set_icon_from_pixbuf(image)
-		n.show()
+            message = "<b>" + p_artist + "</b> - " + p_title
+            n = pynotify.Notification( "MPX", message)
+            n.set_urgency(pynotify.URGENCY_NORMAL)
+            image = m.get_image()
+            if(image):
+                n.set_icon_from_pixbuf(image)
+            n.show()
 
