58756f001437f2b34a3ab7e8d04fe2a519acae81
milosz@HAL <milosz@HAL>
2008-04-18 00:17:42 +0200

    * Add working version of OSD plugin

---
+++ python/plugins/osd/__init__.py
@@ -0,0 +1,173 @@
+
+# -*- coding: utf-8 -*-
+# -*- mode:python ; tab-width:4 -*- ex:set tabstop=4 shiftwidth=4 expandtab: -*-
+#
+# MPX Trackinfo
+# (C) 2008 M. Derezynski
+#
+
+import mpx
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gtk.glade
+import gobject
+import cairo
+import pango
+
+class OSD(gtk.Window):
+
+        def __init__(self):
+
+            gtk.Window.__init__(self, type=gtk.WINDOW_POPUP)
+            self.INNER_PADDING = 8
+            self.cover = None
+            self.metadata = None
+            self.m_artist = ""
+            self.m_title = ""
+            self.m_album = ""
+            self.set_colormap(gtk.gdk.screen_get_default().get_rgba_colormap())
+            self.set_size_request(self.INNER_PADDING*2 + 128 + self.INNER_PADDING + 300, self.INNER_PADDING*2 + 128)
+            screen = gtk.gdk.screen_get_default()
+            self.move((screen.get_width() - (self.INNER_PADDING*3+428)) / 2, (screen.get_height() - (self.INNER_PADDING*2+128))/2)
+    
+        def rounded_rect(self, cr, x, y, width, height, radius):
+
+            edge_length_x = width - radius * 2;
+            edge_length_y = height - radius * 2;
+
+            cr.move_to (x + radius, y);
+            cr.rel_line_to (edge_length_x, 0);
+            cr.rel_curve_to (radius, 0, radius, radius, radius, radius);
+            cr.rel_line_to (0, edge_length_y);
+            cr.rel_curve_to (0, radius, -radius, radius, -radius, radius);
+            cr.rel_line_to (-edge_length_x, 0);
+            cr.rel_curve_to (-radius, 0, -radius, -radius, -radius, -radius);
+            cr.rel_line_to (0, -edge_length_y);
+            cr.rel_curve_to (0, -radius, radius, -radius, radius, -radius);
+
+        def set_metadata(self, m):
+
+            self.metadata = m
+
+            if m[mpx.AttributeId.TITLE].is_initialized():
+                self.m_title = m[mpx.AttributeId.TITLE].val().get_string()
+            else:
+                self.m_title = "(No Title)"
+
+            if m[mpx.AttributeId.ARTIST].is_initialized():
+                self.m_artist = m[mpx.AttributeId.ARTIST].val().get_string()
+            else:
+                self.m_artist = "(Unknown Artist)"
+
+            if m[mpx.AttributeId.ALBUM].is_initialized():
+                self.m_album = m[mpx.AttributeId.ALBUM].val().get_string()
+            else:
+                self.m_album = "(Unknown Album)"
+
+            self.cover = m.get_image().scale_simple(128,128, gtk.gdk.INTERP_HYPER) 
+            self.queue_draw()
+
+        def clear(self):
+
+            self.cover = None
+            self.metadata = None
+            self.queue_draw()
+
+        def do_expose_event(self, event):
+
+            x, y, width, height = self.allocation
+
+            cr = self.window.cairo_create()
+            cr.set_operator(cairo.OPERATOR_CLEAR)
+            cr.rectangle(x, y, width, height) 
+            cr.fill()
+
+            cr.set_operator(cairo.OPERATOR_SOURCE)
+            self.rounded_rect(cr, x, y, width, height, 8.)
+            cr.set_source_rgba(1., 1., 1., 0.8)
+            cr.fill_preserve()
+            cr.set_source_rgba(0., 0., 0., 0.6)
+            cr.stroke()
+
+            if(self.cover):
+            
+                cr.set_source_pixbuf(self.cover, self.INNER_PADDING, self.INNER_PADDING)
+                self.rounded_rect(cr, x + self.INNER_PADDING, y + self.INNER_PADDING, 128, 128, 8.)
+                cr.fill()
+
+            layouts = [ self.create_pango_layout(self.m_artist),
+                        self.create_pango_layout(self.m_album),
+                        self.create_pango_layout(self.m_title)  ]
+
+            positions = [ [128+self.INNER_PADDING, self.INNER_PADDING+28],
+                          [128+self.INNER_PADDING, self.INNER_PADDING+24+28],
+                          [128+self.INNER_PADDING, self.INNER_PADDING+48+28] ]
+
+            for layout in layouts:
+                layout.set_font_description(pango.FontDescription("Sans Serif 14"))
+
+            cr.set_source_rgba(0., 0., 0., 1.)
+            cr.set_operator(cairo.OPERATOR_SOURCE)
+
+            lwidth = width - (3*self.INNER_PADDING) - 128
+      
+            for layout in layouts: 
+                fontw, fonth = layout.get_pixel_size()
+                position = positions[layouts.index(layout)]
+                cr.move_to(position[0] + (lwidth/2 - fontw/2), position[1])
+                cr.update_layout(layout)
+                cr.show_layout(layout)
+    
+            return True
+
+class MPXOSD(mpx.Plugin):
+
+    def __init__(self):
+   
+        gobject.type_register(OSD)
+        self.osd = OSD()
+        self.osd.connect("button-press-event", self.on_button_press)
+        self.timeout_id = None
+
+    def activate(self,player,mcs):
+
+        self.player = player
+        self.player_new_track_handler_id = self.player.gobj().connect("new-track", self.new_track)
+        self.player_state_change_handler_Id = self.player.gobj().connect("play-status-changed", self.pstate_changed)
+        return True
+
+    def deactivate(self):
+        self.player.disconnect(self.player_new_track_handler_id)
+        self.player = None
+
+    def on_button_press(self, widget):
+
+        if(self.timeout_id): 
+            gobject.source_remove(self.timeout_id)
+        hide_osd()
+
+    def hide_osd(self): 
+
+        self.osd.hide()
+        self.timeout_id = None
+        return False
+
+    def pstate_changed(self, playergobj, state): 
+
+        if state == mpx.PlayStatus.STOPPED:
+
+            self.osd.clear()
+            self.osd.hide()
+            if(self.timeout_id):
+                gobject.source_remove(self.timeout_id)
+
+    def new_track(self, playergobj):
+       
+        if(self.timeout_id): 
+            gobject.source_remove(self.timeout_id)
+
+        m = self.player.get_metadata()
+        self.osd.set_metadata(m)
+        self.osd.show_all()
+        self.timeout_id = gobject.timeout_add(5000, self.hide_osd) 
