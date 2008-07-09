132aa269aa8e22e88bc3d5237b055ae62310e11c
milosz@HAL <milosz@HAL>
2008-07-10 01:51:44 +0200

    * Fix string appending bug in various print* functions printing MB ARs into human readable format * Change progress screen to "LOADING"

--- mpx/lyrics-v2.cc
+++ mpx/lyrics-v2.cc
@@ -30,163 +30,17 @@
 #include <fstream>
 
 #include <boost/format.hpp>
-
-#include <libxml/tree.h>
-#include <libxml/parser.h>
 #include <glibmm.h>
 
 #include "mpx/uri.hh"
 #include "mpx/util-string.hh"
+#include "mpx/xml.hh"
 
 using namespace Glib;
 using boost::format;
 using std::string;
 
 #include "lyrics-v2.hh"
-
-namespace
-{
-  enum ElementLyrics
-  {
-    E_LYRICS_NONE                = 0,
-    E_LYRICS_LYRICS              = 1 << 0,
-  };
-
-  struct LyricsParserContext
-  {
-    std::string  & m_lyrics;
-    int            m_state;
-    bool           m_element;
-
-    LyricsParserContext (std::string & lyrics)
-        : m_lyrics  (lyrics),
-          m_state   (0),
-          m_element (false)
-    {}
-  };
-
-  typedef std::map < std::string, std::string > ElementAttributes;
-
-#define SET_STATE(e)    ((context->m_state |= e))
-#define CLEAR_STATE(e)  ((context->m_state &= ~e))
-#define STATE(e)        ((context->m_state & e) != 0)
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  void
-  lyrics_start_element (void*           ctxptr,
-                        xmlChar const*  _name,
-                        xmlChar const** _attributes)
-  {
-    LyricsParserContext* context = static_cast<LyricsParserContext*> (ctxptr);
-    std::string name (reinterpret_cast<char const*> (_name));
-    if (name == "lyrics")
-    {
-      SET_STATE(E_LYRICS_LYRICS);
-      return;
-    }
-  }
-
-  void
-  lyrics_end_element (void*          ctxptr,
-                      xmlChar const* _name)
-  {
-    LyricsParserContext* context = static_cast<LyricsParserContext *> (ctxptr);
-    std::string name (reinterpret_cast<char const*> (_name));
-    if (name == "lyrics")
-    {
-      CLEAR_STATE(E_LYRICS_LYRICS);
-      return;
-    }
-  }
-
-  void
-  lyrics_pcdata (void*          ctxptr,
-                 xmlChar const* _text,
-                 int            length)
-  {
-    LyricsParserContext* context = static_cast<LyricsParserContext *> (ctxptr);
-
-    if (STATE(E_LYRICS_LYRICS))
-      context->m_lyrics.append ((const char*)(_text), length);
-  }
-
-  ////// Common /////////////////////////////////////////////////////////////////
-
-  xmlEntityPtr
-  get_entity (void*          ctxptr,
-              xmlChar const* name)
-  {
-    return xmlGetPredefinedEntity (name);
-  }
-
-  void
-  whitespace (void*          ctxptr,
-              xmlChar const* _text,
-              int             length)
-  {}
-
-  void
-  warning (void*       ctxptr,
-           char const* message,
-           ...)
-  {
-    va_list args;
-    va_start(args, message);
-    g_logv("LyricWiki", G_LOG_LEVEL_WARNING, message, args);
-    va_end(args);
-  }
-
-  void
-  error (void*       ctxptr,
-         char const* message,
-         ...)
-  {
-    va_list args;
-    va_start(args, message);
-    g_logv("LyricWiki", G_LOG_LEVEL_CRITICAL, message, args);
-    va_end(args);
-  }
-
-  void
-  fatal_error (void*       ctxptr,
-               char const* message,
-               ...)
-  {
-    va_list args;
-    va_start(args, message);
-    g_logv("LyricWiki", G_LOG_LEVEL_CRITICAL, message, args);
-    va_end(args);
-  }
-
-  xmlSAXHandler LyricsParser =
-  {
-      (internalSubsetSAXFunc)0,                           // internalSubset
-      (isStandaloneSAXFunc)0,                             // isStandalone
-      (hasInternalSubsetSAXFunc)0,                        // hasInternalSubset
-      (hasExternalSubsetSAXFunc)0,                        // hasExternalSubset
-      (resolveEntitySAXFunc)0,                            // resolveEntity
-      (getEntitySAXFunc)get_entity,                       // getEntity
-      (entityDeclSAXFunc)0,                               // entityDecl
-      (notationDeclSAXFunc)0,                             // notationDecl
-      (attributeDeclSAXFunc)0,                            // attributeDecl
-      (elementDeclSAXFunc)0,                              // elementDecl
-      (unparsedEntityDeclSAXFunc)0,                       // unparsedEntityDecl
-      (setDocumentLocatorSAXFunc)0,                       // setDocumentLocator
-      (startDocumentSAXFunc)0,                            // startDocument
-      (endDocumentSAXFunc)0,                              // endDocument
-      (startElementSAXFunc)lyrics_start_element,          // startElement
-      (endElementSAXFunc)lyrics_end_element,              // endElement
-      (referenceSAXFunc)0,                                // reference
-      (charactersSAXFunc)lyrics_pcdata,                   // characters
-      (ignorableWhitespaceSAXFunc)whitespace,             // ignorableWhitespace
-      (processingInstructionSAXFunc)0,                    // processingInstruction
-      (commentSAXFunc)0,                                  // comment
-      (warningSAXFunc)warning,                            // warning
-      (errorSAXFunc)error,                                // error
-      (fatalErrorSAXFunc)fatal_error,                     // fatalError
-  };
-}
 
 namespace MPX
 {
@@ -205,9 +59,9 @@ namespace MPX
 	std::string
     TextRequest::run ()
     {
-        boost::format lyrics_url_f ("http://lyricwiki.org/api.php?fmt=text&artist=%s&song=%s");
-
+        boost::format lyrics_url_f ("http://lyricwiki.org/api.php?fmt=xml&artist=%s&song=%s");
         URI request_url ((lyrics_url_f % m_artist.c_str() % m_title.c_str()).str(), true);
+
         m_soup_request = Soup::RequestSync::create ((ustring(request_url)));
         m_soup_request->add_header("User-Agent", "BMP-2.0");
         m_soup_request->add_header("Content-Type", "text/xml"); 
@@ -218,21 +72,25 @@ namespace MPX
 
         int code = m_soup_request->run ();
 		if( code != 200)
+        {
 			throw LyricsReturnNotOK();
+        }
 
-		std::string data /*, lyrics*/;
-		m_soup_request->get_data(data);
+		std::string lyrics;
 
-#if 0
-		LyricsParserContext context (lyrics);
-		int rc = xmlSAXUserParseMemory (&LyricsParser, reinterpret_cast<void *>(&context), data.c_str(), data.size()); 
-
-		if(rc)
-			throw LyricsReturnNotOK();
+        try{
+                lyrics = xpath_get_text(
+                    m_soup_request->get_data_raw(),
+                    m_soup_request->get_data_size(),
+                    "//lyrics",
+                    NULL
+                );
+        } catch( ... )
+        {
+            throw LyricsReturnNotOK();
+        }
 
 		return lyrics;
-#endif
-        return data;
     }
 
     TextRequest::~TextRequest ()
