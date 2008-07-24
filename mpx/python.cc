45b4c1643210208d56f60100b9ce1f0c6dc266e5
milosz@HAL <milosz@HAL>
2008-07-24 21:50:39 +0200

    * Adapt Python api to change from r709

--- mpx/python.cc
+++ mpx/python.cc
@@ -845,17 +845,17 @@ BOOST_PYTHON_MODULE(mpx)
 
 	class_<MPX::Library, boost::noncopyable>("Library", boost::python::no_init)
 
-		.def("getSQL",                  &MPX::Library::getSQL)
-		.def("execSQL",                 &MPX::Library::execSQL)
-		.def("getMetadata",             &MPX::Library::getMetadata)
-        .def("sqlToTrack",              &MPX::Library::sqlToTrack)
-        .def("getTrackTags",            &MPX::Library::getTrackTags)
-        .def("albumRated",              &MPX::Library::albumRated)
-        .def("trackRated",              &MPX::Library::trackRated)
-        .def("trackPlayed",             &MPX::Library::trackPlayed) // can't see how plugins could possibly need this
-        .def("trackTagged",             &MPX::Library::trackTagged)
-        .def("markovUpdate",            &MPX::Library::markovUpdate)
-        .def("markovFind",              &MPX::Library::markovFind)
+		.def("getSQL",                          &MPX::Library::getSQL)
+		.def("execSQL",                         &MPX::Library::execSQL)
+		.def("getMetadata",                     &MPX::Library::getMetadata)
+        .def("sqlToTrack",                      &MPX::Library::sqlToTrack)
+        .def("getTrackTags",                    &MPX::Library::getTrackTags)
+        .def("albumRated",                      &MPX::Library::albumRated)
+        .def("trackRated",                      &MPX::Library::trackRated)
+        .def("trackPlayed",                     &MPX::Library::trackPlayed) // can't see how plugins could possibly need this
+        .def("trackTagged",                     &MPX::Library::trackTagged)
+        .def("markovUpdate",                    &MPX::Library::markovUpdate)
+        .def("markovGetRandomProbableTrack",    &MPX::Library::markovGetRandomProbableTrack)
 
         .def("signal_new_album",        &MPX::Library::signal_new_album, return_value_policy<return_by_value>())
         .def("signal_new_artist",       &MPX::Library::signal_new_artist, return_value_policy<return_by_value>())
