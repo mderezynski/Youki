b4e3a3f64c318d119fa127cb1a8bccfb2b9ae552
Milosz Derezynski <internalerror@gmail.com>
2009-11-18 18:20:36 +0100

    * Add missing C GObject marshaler files

---
+++ widgets/widget-marshalers.h
@@ -0,0 +1,28 @@
+
+#ifndef __g_cclosure_user_marshal_MARSHAL_H__
+#define __g_cclosure_user_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:UINT,UINT (widget-marshalers.list:1) */
+extern void g_cclosure_user_marshal_VOID__UINT_UINT (GClosure     *closure,
+                                                     GValue       *return_value,
+                                                     guint         n_param_values,
+                                                     const GValue *param_values,
+                                                     gpointer      invocation_hint,
+                                                     gpointer      marshal_data);
+
+/* VOID:OBJECT,OBJECT (widget-marshalers.list:2) */
+extern void g_cclosure_user_marshal_VOID__OBJECT_OBJECT (GClosure     *closure,
+                                                         GValue       *return_value,
+                                                         guint         n_param_values,
+                                                         const GValue *param_values,
+                                                         gpointer      invocation_hint,
+                                                         gpointer      marshal_data);
+
+G_END_DECLS
+
+#endif /* __g_cclosure_user_marshal_MARSHAL_H__ */
+
